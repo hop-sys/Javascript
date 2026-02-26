16a918a53a005388d281aedefe3ad4b4bd4b1efd
hop-sys <hope28224@gmail.com>
2026-02-26 09:03:55 +0300

    added codes

---
+++ assignment1.js
@@ -0,0 +1,17 @@
+// research and come up with examples on object and array data types. Show how to accesss data types of an array.
+// Array : An array is a data structure that can hold multiple values of different data types.
+// Arrays use index numbers (starting from 0).
+let fruits = ["Apple", "Mango", "Banana"];
+console.log(fruits[0])  
+console.log(fruits[1])  
+console.log(fruits[2])  
+console.log(typeof fruits)
+
+// Object : An object is a collection of properties, where each property is a key-value pair.
+let student = {
+    name: "Hope",
+    age: 16,
+    isActive: true
+}
+console.log(student.name)
+console.log(student.age)    
