3f5b21302ab211703fd2fed1d15cc9eb68e82e2f
hop-sys <hope28224@gmail.com>
2026-02-27 13:26:00 +0300

    added codes

--- assignment3.js
+++ assignment3.js
@@ -1,2 +1,4 @@
 // Create a for loop that is able to print out all the leap years from 2000 to 2026
 // task on page 28 and page 30
+// Research on arrow functions in javascript.
+// Research on functions with parameters and anonymous functions in javascript.
