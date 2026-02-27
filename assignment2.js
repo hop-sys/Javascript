0d8ac5f0c7a8f7939281b3149decdec76a14b405
hop-sys <hope28224@gmail.com>
2026-02-27 10:13:28 +0300

    added codes

--- assignment2.js
+++ assignment2.js
@@ -1,1 +1,15 @@
-// if-else-if-else statement
+// if else statement
+
+let distance = 350
+if (distance <= 100){
+    console.log("Pay 5 USD")
+}
+else if (distance >100 && distance <= 500){
+    console.log("Pay 10 USD")
+}
+else if (distance > 500 && distance <= 1000){
+    console.log("Pay 20 USD")
+}
+else{
+    console.log("Pay 40 USD")
+}
